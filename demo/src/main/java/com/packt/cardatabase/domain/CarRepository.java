837b86740ed352a3f5eee28a04171e804b56ed42
kshkshksh <ksh931101@naver.com>
2023-11-23 13:40:44 +0900

    first

---
+++ demo/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -0,0 +1,16 @@
+package com.packt.cardatabase.domain;
+	
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarRepository extends CrudRepository<Car, Long>{
+	// 브랜드로 차 검색
+	List<Car> findByBrand(String brand);
+	
+	// 색상으로 차 검색
+	List<Car> findByColor(String color);
+	
+	// 연도로 차 검색
+	List<Car> findByYear(int year);
+}
